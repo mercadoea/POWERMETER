db56d11ebc730b585e73c02ef1dcc5f5ae9da4ec
Alberto Mercado <73907682+mercadoea@users.noreply.github.com>
2022-04-27 17:21:00 -0500

    Add files via upload

---
+++ final1.py
@@ -0,0 +1,31 @@
+import sys
+import smbus2 as smbus
+import time
+import struct
+bus = smbus.SMBus(1)
+I2C_SLAVE_ADDR = 0x04
+
+def get_data():
+    data = bus.read_byte_data(I2C_SLAVE_ADDR, 0x00)
+    return data
+
+def get_float(data, index):
+    bytes = data[4*index:(index+1)*4]
+    return struct.unpack('f', "".join(map(chr, bytes)))[0]
+
+def main():
+    print("1: Voltaje \n2: Corriente\n3: Potencia")
+    number = input("Digite el valor asociado al comando: ")
+    bus.write_byte(I2C_SLAVE_ADDR, 0x01, int(number))
+    print("\n")
+    print("Arduino answer to RPI: ", get_data())
+    print("\n")
+    time.sleep(1)
+
+try:
+    while True:
+        main()
+except KeyboardInterrupt:
+    print("\n")
+    print("Exiting...")
+    sys.exit(0)
