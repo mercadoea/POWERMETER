adef5ee311117db4b8589e7cc7cd8610a6041d0b
Cristian Martinez <cdturizo15@gmail.com>
2022-04-28 20:44:52 -0500

    Se creo funcion para dar formato a la data

--- final1.py
+++ final1.py
@@ -1,7 +1,7 @@
 import sys
 from smbus2 import SMBus, i2c_msg
 import time
-import struct
+import struct 
 import smbus2
 bus = smbus2.SMBus(1)
 I2C_SLAVE_ADDR = 0x04
@@ -13,6 +13,14 @@ def get_data():
         data1 = list(msg)
     return data1
 
+def formated_data(data):
+    smsMessage = ""
+    for i in data:
+        smsMessage += chr(i)
+    clean_data = struct.unpack('<f',smsMessage.encode())
+    return clean_data
+
+
 def get_float(data, index):
     bytes = data[4*index:(index+1)*4]
     return struct.unpack('f', "".join(map(chr, bytes)))[0]
@@ -23,7 +31,9 @@ def main():
     bus.write_byte(I2C_SLAVE_ADDR, int(number))
     print("\n")
     print("Arduino answer to RPI: ", get_data())
+    data = get_data()
     print("\n")
+    print(f'Arduino ansewer: {formated_data(data)}')
     time.sleep(1)
 
 try:
