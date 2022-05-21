8a577dad585a7be4995c748c101825f24cfaf4d2
Cristian Martinez <cdturizo@uninorte.edu.co>
2022-05-21 13:39:16 -0500

    Libreria Modular

--- final1.py
+++ final1.py
@@ -4,17 +4,17 @@ import time
 import struct 
 import smbus2
 
-bus = smbus2.SMBus(1)
-I2C_SLAVE_ADDR = 0x04
+__bus = smbus2.SMBus(1)
+__I2C_SLAVE_ADDR = 0x04
 
-def get_data():
+def __get_data():
     with SMBus(1) as bus:
-        msg = i2c_msg.read(I2C_SLAVE_ADDR, 4)
-        bus.i2c_rdwr(msg)
+        msg = i2c_msg.read(__I2C_SLAVE_ADDR, 4)
+        __bus.i2c_rdwr(msg)
         data1 = list(msg)
     return data1
 
-def get_ieee(data):
+def __get_ieee(data):
     ieee = ''
     data.reverse()
     for d in data:
@@ -24,30 +24,63 @@ def get_ieee(data):
         ieee+= byte    
     return ieee
 
-def ieee745ToFloat(N): # ieee-745 bits (max 32 bit)
+def __ieee745ToFloat(N): # ieee-745 bits (max 32 bit)
     a = int(N[0])        # sign,     1 bit
     b = int(N[1:9],2)    # exponent, 8 bits
     c = int("1"+N[9:], 2)# fraction, len(N)-9 bits
 
     return (-1)**a * c /( 1<<( len(N)-9 - (b-127) ))
 
-
-def main():
-    print("1: Voltaje \n2: Corriente\n3: Potencia")
-    number = input("Digite el valor asociado al comando: ")
-    bus.write_byte(I2C_SLAVE_ADDR, int(number))
+def getVoltage():
+    number = 1
+    __bus.write_byte(__I2C_SLAVE_ADDR, int(number))
+    print("Arduino answer to RPI: ", __get_data())
+    ieee_data = __get_ieee(__get_data())
+    data = __ieee745ToFloat(ieee_data)
     print("\n")
-    print("Arduino answer to RPI: ", get_data())
-    ieee_data = get_ieee(get_data())
-    data = ieee745ToFloat(ieee_data)
-    print("\n")
-    print(f'Arduino ansewer: {data}')
+    print(f'Arduino answer: {data}')
     time.sleep(1)
 
-try:
+def getCurrent():
+    number = 2
+    __bus.write_byte(__I2C_SLAVE_ADDR, int(number))
+    print("Arduino answer to RPI: ", __get_data())
+    ieee_data = __get_ieee(__get_data())
+    data = __ieee745ToFloat(ieee_data)
+    print("\n")
+    print(f'Arduino answer: {data}')
+    time.sleep(1)
+
+def getPower():
+    number = 3
+    __bus.write_byte(__I2C_SLAVE_ADDR, int(number))
+    print("Arduino answer to RPI: ", __get_data())
+    ieee_data = __get_ieee(__get_data())
+    data = __ieee745ToFloat(ieee_data)
+    print("\n")
+    print(f'Arduino answer: {data}')
+    time.sleep(1)
+
+def main():
+    try:
+        print("1: Voltaje \n2: Corriente\n3: Potencia")
+        number = input("Digite el valor asociado al comando: ")
+        __bus.write_byte(__I2C_SLAVE_ADDR, int(number))
+        print("\n")
+        print("Arduino answer to RPI: ", __get_data())
+        ieee_data = __get_ieee(__get_data())
+        data = __ieee745ToFloat(ieee_data)
+        print("\n")
+        print(f'Arduino ansewer: {data}')
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print('\n')
+        print('Exiting....')
+
+""" try:
     while True:
         main()
 except KeyboardInterrupt:
     print("\n")
     print("Exiting...")
-    sys.exit(0)
+    sys.exit(0)  """
